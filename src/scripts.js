17bfee8f6691576b20fc7596b5327ded1f479010
Maleke-Bridget-Nkwana <166614093+Maleke-Bridget-Nkwana@users.noreply.github.com>
2024-12-23 10:03:26 +0200

    Added quantity tracking to cart

--- src/scripts.js
+++ src/scripts.js
@@ -3,8 +3,13 @@ let cart = [];
 
 // Function to Add to Cart
 function addToCart(productName, productPrice) {
-  let product = { name: productName, price: productPrice };
-  cart.push(product);
+  let existingProduct = cart.find((item) => item.name === productName);
+  if (existingProduct) {
+    existingProduct.quantity += 1; // Increase quantity if product already exists
+  } else {
+    let product = { name: productName, price: productPrice, quantity: 1 };
+    cart.push(product);
+  }
   alert(`${productName} has been added to the cart!`);
   updateCartDisplay();
 }
@@ -12,24 +17,41 @@ function addToCart(productName, productPrice) {
 // Function to Display Cart Contents
 function updateCartDisplay() {
   let cartContainer = document.querySelector(".cart-container");
+  let cartTotal = document.querySelector(".cart-total");
   cartContainer.innerHTML = ""; // Clear previous content
+
   if (cart.length === 0) {
     cartContainer.innerHTML = "<p>Your cart is empty.</p>";
+    cartTotal.textContent = "Total: R0.00";
   } else {
+    let totalPrice = 0;
     cart.forEach((item, index) => {
+      totalPrice += item.price * item.quantity;
       cartContainer.innerHTML += `
                 <div class="cart-item">
-                    <span>${item.name} - R${item.price}</span>
+                    <span>${item.name} - R${item.price} x ${item.quantity}</span>
+                    <button onclick="increaseQuantity(${index})">+</button>
+                    <button onclick="decreaseQuantity(${index})">-</button>
                     <button onclick="removeFromCart(${index})">Remove</button>
                 </div>
             `;
     });
+    cartTotal.textContent = `Total: R${totalPrice.toFixed(2)}`;
   }
 }
 
-// Function to Remove Item from Cart
-function removeFromCart(index) {
-  let removedItem = cart.splice(index, 1)[0];
-  alert(`${removedItem.name} has been removed from the cart!`);
+// Function to Increase Quantity
+function increaseQuantity(index) {
+  cart[index].quantity += 1;
+  updateCartDisplay();
+}
+
+// Function to Decrease Quantity
+function decreaseQuantity(index) {
+  if (cart[index].quantity > 1) {
+    cart[index].quantity -= 1;
+  } else {
+    removeFromCart(index);
+  }
   updateCartDisplay();
 }
